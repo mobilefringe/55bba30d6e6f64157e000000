51ee6fe4a38dd7241e6613fe232f5ad17c45232a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-25 16:59:38 -0400

    Midtown Plaza 1.4  /application.js

--- application.js
+++ application.js
@@ -708,8 +708,8 @@ function renderHomeHours(container, template, collection){
     $.each( item_list , function( key, val ) {
         val.day = get_day(val.day_of_week);
         var d = moment().tz(getPropertyTimeZone());
-        val.month = d.format("MMM")
-        val.weekday = d.format("dd")
+        val.month = d.format("MMM");
+        val.weekday = d.format("dd");
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = in_my_time_zone(moment(val.open_time), "h:mma");
             var close_time = in_my_time_zone(moment(val.close_time), "h:mma");
