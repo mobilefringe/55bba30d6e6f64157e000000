f7793ccf2b38afda794b3762dee2984ccca53f38
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-25 17:05:32 -0400

    Midtown Plaza 1.4  /application.js

--- application.js
+++ application.js
@@ -208,8 +208,8 @@ function renderHours(container, template, collection, type){
                         break;
                 }
                 if (val.open_time && val.close_time && val.is_closed == false){
-                    var open_time = in_my_time_zone(moment(val.open_time), "h:mmA");
-                    var close_time = in_my_time_zone(moment(val.close_time), "h:mmA");
+                    var open_time = in_my_time_zone(moment(val.open_time), "h:mm a");
+                    var close_time = in_my_time_zone(moment(val.close_time), "h:mm a");
                     val.h = open_time + " - " + close_time;
                 } else {
                     "Closed";
@@ -226,7 +226,7 @@ function renderHours(container, template, collection, type){
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == true) {
                 holiday = moment(val.holiday_date);
-                val.formatted_date = in_my_time_zone(holiday, "MMM D");
+                val.formatted_date = in_my_time_zone(holiday, "MMMM D");
                 if (val.open_time && val.close_time && val.is_closed == false){
                     var open_time = in_my_time_zone(moment(val.open_time), "h:mm a");
                     var close_time = in_my_time_zone(moment(val.close_time), "h:mm a");
