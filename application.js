69b7bc2eb90a0550016ad8e3bd75f402d29c624e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-31 10:27:48 -0400

    Midtown Plaza 1.7  /application.js

--- application.js
+++ application.js
@@ -365,7 +365,7 @@ function renderPromosEvents(container, template, collection){
     $.each( collection , function( key, val ) {
         if (val.type=="promotions"){
             val.image_url = val.promo_image_url_abs;
-            if((val.promo_image_url).indexOf('missing.png') > -1){
+            if(val.promo_image_url_abs == null || (val.promo_image_url_abs).indexOf('missing.png') > -1){
                 if (val.promotionable_type == "Store") {
                     var store_details = getStoreDetailsByID(val.promotionable_id);
                     val.store_detail_btn = store_details.slug ;
