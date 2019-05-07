6d6165708a754e6edaa0fe8ccb9a5b52f1e21e59
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-05-07 13:58:16 -0400

    Midtown Plaza 1.7  /helpers.js

--- helpers.js
+++ helpers.js
@@ -321,7 +321,7 @@ function floorList() {
     return floor_list;
 }
 
-function drop_pin(id){
+function drop_pin(id,map){
     map.marksHide();
     var coords = map.get_coords(id);
     var height = parseInt(coords["height"]);
