8edd3f0cdb802848680fd935759040164bc5a24e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-25 15:35:43 -0400

    Midtown Plaza 1.7  /helpers.js

--- helpers.js
+++ helpers.js
@@ -326,7 +326,7 @@ function floorList() {
     var floor_1 = {};
     floor_1.id = "first-floor";
     floor_1.title = "Level One";
-    floor_1.map =  getSVGMapURL.split("?")[0];
+    floor_1.map =  getSVGMapURL().split("?")[0];
     // getSVGMapURL().split("?")[0];
     floor_1.z_index = 1;
     floor_1.show = true;
