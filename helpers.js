20a79d7d5a86fa6a4c7ebe8dca03c9331a9fe546
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-10-21 10:36:43 -0400

    Midtown Plaza 1.7  /helpers.js

--- helpers.js
+++ helpers.js
@@ -65,7 +65,7 @@ function show_content(){
     $(".modal-backdrop").remove();
 }
 
-function pinIt(){
+function pinIt() {
     var e = document.createElement('script');
     e.setAttribute('type','text/javascript');
     e.setAttribute('charset','UTF-8');
